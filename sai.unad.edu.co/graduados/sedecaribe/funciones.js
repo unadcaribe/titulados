2601943c73d04d28a6a26eaeece3a2834e05fb10
unadcaribe <seccional.atlantico@engineer.com>
2025-03-16 09:34:49 -0500

    Add files via upload

---
+++ sai.unad.edu.co/graduados/sedecaribe/funciones.js
@@ -0,0 +1,94 @@
+// JavaScript Document
+//Desarrollado por Elias David Niño Ruiz
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function validar(a,b)
+{
+	    
+
+        if(a == "15887" && b == "1116504502")
+	{
+		document.location= "15887.html";
+	} 
+	 
+       	
+        if(a == "4566741" && b == "1116504502")
+	{
+		document.location= "4566741.html";
+	}
+	
+	        	
+	 if(a == "15883" && b == "1116492134")
+	{
+		document.location= "15883.html";
+	}
+
+         if(a == "4566734" && b == "1116492134")
+	{
+		document.location= "4566734.html";
+	}
+
+
+         if(a == "13423" && b == "39650072")
+	{
+		document.location= "13423.html";
+	}
+
+         if(a == "4566865" && b == "39650072")
+	{
+		document.location= "4566865.html";
+	}
+
+        if(a == "12624159" || b == "12624159")
+	{
+		document.location= "14421.htm";
+	}
+
+        if(a == "79613100" || b == "1055221927")
+	{
+		document.location= "3100.htm";
+	}
+        
+        if(a == "72248067" || b == "72248067")
+	{
+		document.location= "1425.htm";
+	}
+
+        if(a == "72220127" || b == "72220127")
+	{
+		document.location= "72220127.htm";
+	}
+
+         if(a == "8633712" || b == "1071487341")
+	{
+		document.location= "3712.htm";
+	}
+
+         if(a == "26871307" || b == "26871307")
+	{
+		document.location= "1307.htm";
+	}
+        
+         if(a == "42403910" || b == "42403910")
+	{
+		document.location= "3910.htm";
+	}
+
+  if(a == "42491803" || b == "42491803")
+	{
+		document.location= "4249.html";
+	}
+
+  if(a == "8775883" || b == "8775883")
+	{
+		document.location= "5883.htm";
+	}
+ if(a == "71252350" || b == "71252350")
+	{
+		document.location= "52350.htm";
+	}
+ 
+     
+}
+
