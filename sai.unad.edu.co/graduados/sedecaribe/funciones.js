ce5ef67533c5617efae63e59f7a90fdc16ba6d7c
unadcaribe <seccional.atlantico@engineer.com>
2025-10-01 14:39:53 -0500

    Update funciones.js

--- sai.unad.edu.co/graduados/sedecaribe/funciones.js
+++ sai.unad.edu.co/graduados/sedecaribe/funciones.js
@@ -13,9 +13,8 @@ function validar(a,b)
     if(a == "107143" && b == "1007646510")
 	  {
 		document.location= "107143.html";
-	}
+	  }
 
-{
    if(a == "651" && b == "72131090")
 	  {
 		document.location= "651.html";
@@ -24,11 +23,8 @@ function validar(a,b)
     if(a == "224814" && b == "72131090")
 	  {
 		document.location= "224814.html";
-	}
+	  }
 
-
-
-{
    if(a == "1151" && b == "1116863595")
 	  {
 		document.location= "1151.html";
@@ -37,10 +33,9 @@ function validar(a,b)
     if(a == "107137" && b == "1116863595")
 	  {
 		document.location= "107137.html";
-	}
+	  }
 	
-
-		
+	
 	if(a == "1142" && b == "88033756")
 	  {
 		document.location= "1142.html";
@@ -50,8 +45,6 @@ function validar(a,b)
 	  {
 		document.location= "107132.html";
 	  }
-
-
 	
 	  if(a == "13280" && b == "1115735988")
 	    {
@@ -76,113 +69,103 @@ function validar(a,b)
 	      }
 
 
-
-
- if(a == "15886" && b == "1046399856")
-	{
-		document.location= "15886.html";
-	} 
+        if(a == "15886" && b == "1046399856")
+	      {
+		   document.location= "15886.html";
+	      } 
 	 
        	
         if(a == "456677" && b == "1046399856")
-	{
-		document.location= "456677.html";
-	}
+	      {
+		   document.location= "456677.html";
+	      }
 
-
-
-
-	
-	
-
-
-
-   
 
         if(a == "15887" && b == "1116504502")
-	{
-		document.location= "15887.html";
-	} 
+	      {
+		   document.location= "15887.html";
+	      } 
 	 
        	
         if(a == "4566741" && b == "1116504502")
-	{
-		document.location= "4566741.html";
-	}
+	      {
+		   document.location= "4566741.html";
+	      }
 	
 	        	
-	 if(a == "15883" && b == "1116492134")
-	{
-		document.location= "15883.html";
-	}
+	    if(a == "15883" && b == "1116492134")
+	      {
+		   document.location= "15883.html";
+	      }
 
          if(a == "4566734" && b == "1116492134")
-	{
-		document.location= "4566734.html";
-	}
+	       {
+		    document.location= "4566734.html";
+	       }
 
 
          if(a == "13423" && b == "39650072")
-	{
-		document.location= "13423.html";
-	}
+	       {
+		    document.location= "13423.html";
+	       }
 
          if(a == "4566865" && b == "39650072")
-	{
-		document.location= "4566865.html";
-	}
+	       {
+		    document.location= "4566865.html";
+	       }
 
         if(a == "12624159" || b == "12624159")
-	{
-		document.location= "14421.htm";
-	}
+	      {
+		   document.location= "14421.htm";
+	      }
 
         if(a == "79613100" || b == "1055221927")
-	{
-		document.location= "3100.htm";
-	}
+	      {
+		   document.location= "3100.htm";
+	      }
         
         if(a == "72248067" || b == "72248067")
-	{
-		document.location= "1425.htm";
-	}
+	      {
+		   document.location= "1425.htm";
+	      }
 
         if(a == "72220127" || b == "72220127")
-	{
-		document.location= "72220127.htm";
-	}
+	      {
+		   document.location= "72220127.htm";
+	      }
 
          if(a == "8633712" || b == "1071487341")
-	{
-		document.location= "3712.htm";
-	}
+	       {
+		    document.location= "3712.htm";
+	       }
 
          if(a == "26871307" || b == "26871307")
-	{
-		document.location= "1307.htm";
-	}
+	       {
+		    document.location= "1307.htm";
+	       }
         
          if(a == "42403910" || b == "42403910")
-	{
-		document.location= "3910.htm";
-	}
+	       {
+		    document.location= "3910.htm";
+	       }
 
-  if(a == "42491803" || b == "42491803")
-	{
-		document.location= "4249.html";
-	}
+        if(a == "42491803" || b == "42491803")
+	      {
+		    document.location= "4249.html";
+	      }
 
-  if(a == "8775883" || b == "8775883")
-	{
-		document.location= "5883.htm";
-	}
- if(a == "71252350" || b == "71252350")
-	{
-		document.location= "52350.htm";
-	}
+        if(a == "8775883" || b == "8775883")
+	      {
+		   document.location= "5883.htm";
+	      }
+        if(a == "71252350" || b == "71252350")
+	     {
+		  document.location= "52350.htm";
+	     }
  
      
 }
+
 
 
 
